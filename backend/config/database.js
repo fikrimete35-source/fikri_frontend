c7014dd7b252e7f6ee51546e9716c605209313d8
bahattin ilkbahar <bahattinilkbahar39@gmail.com>
2026-06-10 17:22:01 +0300

    database fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,31 +1,9 @@
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  if (client === 'postgres') {
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          connectionString: env('DATABASE_URL'),
-          ssl: env.bool('DATABASE_SSL', true) && { rejectUnauthorized: false },
-        },
-        options: {
-          pool: {
-            min: env.int('DATABASE_POOL_MIN', 2),
-            max: env.int('DATABASE_POOL_MAX', 10),
-          },
-        },
-      },
-    };
-  }
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'sqlite',
-      connection: {
-        filename: '.tmp/data.db',
-      },
-      useNullAsDefault: true,
+      connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false }
     },
-  };
-};
+  },
+});
